86c5ab5fa92006af1d15712c94de3b6ae5754231
Cody Brouwers <clbrouwers@gmail.com>
2020-02-28 18:28:10 +0900

    Remove logs from iOS wrapper

--- ios/ButtonSdk.m
+++ ios/ButtonSdk.m
@@ -8,25 +8,25 @@
 RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(setIdentifier:(NSString *)identifier) {
-  RCTLogInfo(@"setIdentifier %@", identifier);
+  // RCTLogInfo(@"setIdentifier %@", identifier);
   [Button.user setIdentifier:identifier];
 }
 
 RCT_EXPORT_METHOD(clearAllData) {
-  RCTLogInfo(@"clearAllData");
+  // RCTLogInfo(@"clearAllData");
   [Button clearAllData];
 }
 
 RCT_EXPORT_METHOD(startPurchasePath:(NSDictionary *)options) {
   NSURL *url = [RCTConvert NSURL:options[@"url"]];
 
-  RCTLogInfo(@"startPurchasePath: url: %@", url);
+  // RCTLogInfo(@"startPurchasePath: url: %@", url);
 
   BTNPurchasePathRequest *request = [BTNPurchasePathRequest requestWithURL:url];
 
   if (options[@"token"]) {
     NSString *token = [RCTConvert NSString:options[@"token"]];
-    RCTLogInfo(@"startPurchasePath: token: %@", token);
+    // RCTLogInfo(@"startPurchasePath: token: %@", token);
     request.pubRef = token;
   }
 
