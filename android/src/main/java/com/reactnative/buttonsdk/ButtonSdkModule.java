c89a8f93071e30ef6240442f738b909430d699c4
Cody Brouwers <clbrouwers@gmail.com>
2020-02-28 18:56:51 +0900

    Update indentation and alignment for Android files

--- android/src/main/java/com/reactnative/buttonsdk/ButtonSdkModule.java
+++ android/src/main/java/com/reactnative/buttonsdk/ButtonSdkModule.java
@@ -7,21 +7,21 @@ import com.facebook.react.bridge.Callback;
 
 public class ButtonSdkModule extends ReactContextBaseJavaModule {
 
-    private final ReactApplicationContext reactContext;
+  private final ReactApplicationContext reactContext;
 
-    public ButtonSdkModule(ReactApplicationContext reactContext) {
-        super(reactContext);
-        this.reactContext = reactContext;
-    }
+  public ButtonSdkModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
 
-    @Override
-    public String getName() {
-        return "ButtonSdk";
-    }
+  @Override
+  public String getName() {
+    return "ButtonSdk";
+  }
 
-    @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
-    }
+  @ReactMethod
+  public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
+    // TODO: Implement some actually useful functionality
+    callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+  }
 }
--- android/src/main/java/com/reactnative/buttonsdk/ButtonSdkPackage.java
+++ android/src/main/java/com/reactnative/buttonsdk/ButtonSdkPackage.java
@@ -11,13 +11,13 @@ import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
 
 public class ButtonSdkPackage implements ReactPackage {
-    @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        return Arrays.<NativeModule>asList(new ButtonSdkModule(reactContext));
-    }
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+    return Arrays.<NativeModule>asList(new ButtonSdkModule(reactContext));
+  }
 
-    @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Collections.emptyList();
-    }
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+    return Collections.emptyList();
+  }
 }
